be1f833fc360d365668785d5a9b9a0255440643c
Prashant yaduvanahi <122152040+prashant6202@users.noreply.github.com>
2024-01-07 11:21:13 +0530

    Add files via upload

---
+++ 9.GUI_PACKAGE_SWING/Test_JFrame_Demo.java
@@ -0,0 +1,16 @@
+
+package GUI_PACKAGE_Swing;
+
+import javax.swing.JFrame;
+
+
+public class Test_JFrame_Demo {
+    public static void main(String[] args) {
+        JFrame jf=new JFrame();
+        jf.setTitle("Prashant`s Frame");
+        jf.setBounds(200, 200,400, 400);
+        jf.setVisible(true);
+        jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+}
