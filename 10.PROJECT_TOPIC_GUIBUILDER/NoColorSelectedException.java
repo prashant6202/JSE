23de165759249fb764d2a28a17aac7df22f53fd3
Prashant yaduvanahi <122152040+prashant6202@users.noreply.github.com>
2024-01-07 11:22:47 +0530

    Add files via upload

---
+++ 10.PROJECT_TOPIC_GUIBUILDER/NoColorSelectedException.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ProjectTopics_GUIBUILDER.apps;
+
+/**
+ *
+ * @author HP
+ */
+public class NoColorSelectedException extends Exception {
+
+    public NoColorSelectedException(String message) {
+        super(message);
+    }
+    
+}
